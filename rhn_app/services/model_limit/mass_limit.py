7beede9ce0e61e9ab3eb285f7c32bf63dc1fab75
abhijit-rout-01 <abhijitrout520@gmail.com>
2025-04-30 22:15:33 +0530

    gradientDescent added

---
+++ rhn_app/services/model_limit/mass_limit.py
@@ -0,0 +1,38 @@
+import pandapipes as pp
+import pandas as pd
+import os
+
+def bestMassFlow(net):
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sourcefile = os.path.join(BASE_DIR, "data", "Data.xlsx")
+    df_sink=pd.read_excel(sourcefile,sheet_name=1)
+    
+    max_total_demand=0
+    max_col=''
+    for col in range(7,len(df_sink.columns)):
+        total_demand=0
+        for j in range(len(df_sink)):
+            total_demand+=float(str(df_sink.at[j,col]))
+        if total_demand>max_total_demand:
+            max_col=df_sink.columns[col]
+            max_total_demand=total_demand
+
+    for j in range(len(net.heat_consumer)):
+        net.heat_consumer.at[j,'qext_w']=float(str(df_sink.at[j,max_col]))*1000
+    
+    for i in range(len(net.circ_pump_pressure)):
+        net.circ_pump_pressure.at[i,'t_flow_k']=373
+    
+    pp.pipeflow(net,mode='sequential')
+    for i in range(0,2):
+        for i in range(len(net.res_junction)):
+            net.junction.at[i,'tfluid_k']=float(str(net.res_junction.at[i,'t_k']))
+            net.junction.at[i,'pn_bar']=float(str(net.res_junction.at[i,'p_bar']))
+        pp.pipeflow(net,mode='sequential')
+    
+    req_mass_flow=[]
+    for i in range(len(net.res_circ_pump_pressure)):
+        req_mass_flow.append(net.res_circ_pump_pressure.at[i,'mdot_flow_kg_per_s'])
+
+    print(req_mass_flow)
+    return req_mass_flow
---
+++ rhn_app/services/network_services/optimized_temp.py
@@ -0,0 +1,71 @@
+import pandapipes as pp
+import pandas as pd
+from rhn_app.services.network_services.edit_sinks import edit_sinks_from_df
+
+def calcPipeFlow(net,T1,T2):
+    net.circ_pump_pressure.at[0,'t_flow_k']=T1
+    net.circ_pump_pressure.at[1,'t_flow_k']=T2
+    for i in range(len(net.junction)):
+        if net.junction.at[i,'name'].endswith('_supply'):
+            net.junction.at[i,'tfluid_k']=min(T1,T2)-5
+    net.junction.loc[net.junction['name']=='Sarfvik_supply','tfluid_k']=T1
+    net.junction.loc[net.junction['name']=='Kirkkonummi_supply','tfluid_k']=T2
+    
+    pp.pipeflow(net,mode='sequential')
+    
+    for i in range(len(net.res_junction)):
+        net.junction.at[i,'tfluid_k']=float(str(net.res_junction.at[i,'t_k']))
+        net.junction.at[i,'pn_bar']=float(str(net.res_junction.at[i,'p_bar']))
+    net.junction
+    pp.pipeflow(net,mode='sequential')
+    
+    return [net.res_circ_pump_pressure.at[0,'mdot_flow_kg_per_s'],net.res_circ_pump_pressure.at[1,'mdot_flow_kg_per_s']]
+
+def gradDescOptimizer(net,
+                     req_mass_flow, 
+                     lower_limit, 
+                     learning_rate=0.3, tolerance=1.0, 
+                     max_iters=1000, delta=0.5):
+    
+    #set demands for current time
+    edit_sinks_from_df(net)
+
+    m1_target=req_mass_flow[0]
+    m2_target=req_mass_flow[1]
+    T1, T2 = lower_limit[0], lower_limit[1]
+    history = []
+
+    for i in range(max_iters):
+        print(T1,T2,end='\t')
+        m1, m2 = calcPipeFlow(net,T1, T2)
+        print(m1,m2)
+        
+        # Compute error
+        err1 = m1 - m1_target
+        err2 = m2 - m2_target
+        error = err1**2 + err2**2
+
+        history.append((T1, T2, m1, m2, error))
+
+        if abs(err1) <= tolerance and abs(err2) <= tolerance:
+            print(f"Converged in {i+1} iterations.")
+            break
+
+        # Numerical gradient w.r.t T1
+        if abs(err1)>tolerance:
+            m1_T1_plus, m2_T1_plus = calcPipeFlow(net,T1 + delta, T2)
+            grad_T1 = ((m1_T1_plus - m1) * err1 + (m2_T1_plus - m2) * err2) / delta
+        else:
+            grad_T1=1.2
+        # Numerical gradient w.r.t T2
+        if abs(err2)>tolerance:
+            m1_T2_plus, m2_T2_plus = calcPipeFlow(net,T1, T2 + delta)
+            grad_T2 = ((m1_T2_plus - m1) * err1 + (m2_T2_plus - m2) * err2) / delta
+        else:
+            grad_T2=1.2
+        # Update temperatures
+        T1 -= learning_rate * grad_T1
+        T2 -= learning_rate * grad_T2
+    
+    optimized_temp={'Sarfvik':T1,'Kirkkonummi':T2}
+    return optimized_temp
