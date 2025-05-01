6b0801e7cf7133cf21a3a8b8f067c095b63e0135
Aditya Mukherjee <adityamukherjee.dgp@gmail.com>
2025-05-01 15:46:56 +0530

    pandapipes-develop error 3

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,11 +1,13 @@
 import os
 import sys
 
-# Path to the custom pandapipes folder that contains pandapipes/
-custom_pandapipes_path = os.path.join(os.path.dirname(__file__), 'custom_pandapipes')
+# Add the directory that contains the 'pandapipes' module to sys.path
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+custom_pandapipes_path = os.path.join(BASE_DIR, 'custom_pandapipes')
 if custom_pandapipes_path not in sys.path:
     sys.path.insert(0, custom_pandapipes_path)
 
+# Django setup
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 from django.core.wsgi import get_wsgi_application
